9447c57f3ea5dba4d1deeea37e5b7d59716a4af5
NickleDave <nicholdav@gmail.com>
2019-03-03 07:23:06 -0500

    fix tests in test_vaktestnet.py

--- tests/test_vaktestnet.py
+++ tests/test_vaktestnet.py
@@ -2,19 +2,19 @@ import unittest
 import inspect
 
 import vak_test_net
-import songdeck
+import vak
 
 
-class TestSongdeckTestNet(unittest.TestCase):
+class TestVakTestNet(unittest.TestCase):
     def setUp(self):
-        self.songdeck_test_net = vak_test_net.SongdeckTestNet()
+        self.vak_test_net = vak_test_net.VakTestNet()
 
     def test_attribs(self):
-        asn = songdeck.network.AbstractSongdeckNetwork()
-        asn_methods = [method[0] for method in inspect.getmembers(asn)
+        avn = vak.network.AbstractVakNetwork()
+        avn_methods = [method[0] for method in inspect.getmembers(avn)
                        if inspect.ismethod(method[1])]
-        for asn_method in asn_methods:
-            self.assertTrue(hasattr(self.songdeck_test_net, asn_method))
+        for avn_method in avn_methods:
+            self.assertTrue(hasattr(self.vak_test_net, avn_method))
 
 
 if __name__ == '__main__':
